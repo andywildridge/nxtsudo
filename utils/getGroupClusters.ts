c406f267e86a08b1d9044c69bf66176055a910e9
andywildridge <andy.wildridge@gmail.com>
2023-01-21 23:43:38 +0000

    xwing

--- utils/getGroupClusters.ts
+++ utils/getGroupClusters.ts
@@ -1,96 +1,132 @@
-import { isObjectBindingPattern } from 'typescript';
-import { Collections, CollectionsGroup } from './collections';
-import { groupIndeces } from './indexTransforms'
- 
-export const getGroupClusters = (possibles:  ReadonlyMap<number, Set<number>>, groupings: Collections) => {
- //function find group clusters
-    //fold groups into row 0, clusters boxes (in lines)?
-    //fold groups into rows/cols, , number n, clusters
-    // func clusters = getClusters(groupings)
-    // reverses position and possibles
-    //console.log(groupings);
-    const clusters = new CollectionsGroup();
-    for (let [key, positions] of groupings.values) {
-        //type,index number/possible clusters
-        clusters.add({
-            value: ~~key.split('.')[1].split(':')[1],
-            type: key.split('.')[0].split(':')[0],
-            index: ~~key.split('.')[0].split(':')[1],
-            positions: [...positions]
+import { isObjectBindingPattern } from "typescript";
+import { isBooleanObject } from "util/types";
+import { Collections, CollectionsGroup } from "./collections";
+import { groupIndeces } from "./indexTransforms";
+
+export const getGroupClusters = (
+  possibles: ReadonlyMap<number, Set<number>>,
+  groupings: Collections
+) => {
+  //function find group clusters
+  //fold groups into row 0, clusters boxes (in lines)?
+  //fold groups into rows/cols, , number n, clusters
+  // func clusters = getClusters(groupings)
+  // reverses position and possibles
+  //console.log(groupings);
+  const clusters = new CollectionsGroup();
+  for (let [key, positions] of groupings.values) {
+    //type,index number/possible clusters
+    clusters.add({
+      value: ~~key.split(".")[1].split(":")[1],
+      type: key.split(".")[0].split(":")[0],
+      index: ~~key.split(".")[0].split(":")[1],
+      positions: [...positions],
+    });
+
+    //type,number index/possible clusters
+    clusters.add({
+      value: ~~key.split(".")[0].split(":")[1],
+      type: `${key.split(".")[0].split(":")[0]}.num`,
+      index: ~~key.split(".")[1].split(":")[1],
+      positions: [...positions],
+    });
+  }
+  // clusters now a map of uniquie possible sets as keys and position distribution as values;
+
+  interface StringMap {
+    [key: string]: any;
+  }
+
+  const groups = [];
+  const singles = [];
+
+  for (let [_key, item] of clusters.values) {
+    //item = possible clusterBlock
+    for (let [_key, obj] of item) {
+      //obj =
+      // only process groups not number groupss here where > 1
+      if (obj.type.indexOf("num") > -1 && obj.positionCluster.length === 1) {
+        continue;
+      } // skip single num as picked up elswheres
+
+      if (obj.positionCluster.length !== obj.canContainNumbers.size) {
+        continue;
+      }
+
+      if (obj.positionCluster.length === 1) {
+        singles.push({
+          square: groupIndeces[obj.type](obj.index)[obj.positionCluster[0]],
+          type: obj.type,
+          number: [...obj.canContainNumbers][0],
+          because: `only place this number can go on this ${obj.type}`,
         });
-        
-        //type,number index/possible clusters
-        clusters.add({
-            value: ~~key.split('.')[0].split(':')[1],
-            type: `${key.split('.')[0].split(':')[0]}.num`,
-            index: ~~key.split('.')[1].split(':')[1],
-            positions: [...positions]
+      }
+
+      if (obj.positionCluster.length > 1 && obj.type.indexOf("num") > -1) {
+        console.log(obj);
+        const type = obj.type.split(".")[0];
+        if(type === 'box') { continue; }
+        const oppositeType = type === 'row' ? 'col' : 'row';
+        const canDelete = [];
+        obj.positionCluster.forEach((opp)=>{
+            const related = groupIndeces[oppositeType](opp).filter((i,idx)=>!obj.canContainNumbers.has(idx) && 
+            [...(possibles.get(i) || [])].includes(obj.index));
+            console.log(related);
         });
-        
-    }
-    // clusters now a map of uniquie possible sets as keys and position distribution as values;
+      }
 
-    interface StringMap { [key: string]: any; }
-
-    const groups = [];
-    const singles = [];
-
-    for(let [_key, item] of clusters.values) { //item = possible clusterBlock
-        for(let [_key, obj] of item) { //obj = 
-            // only process groups not number groupss here where > 1
-            if (
-              obj.type.indexOf("num") > -1 &&
-              obj.positionCluster.length === 1
-            ) {
-              continue;
-            } // skip single num as picked up elswheres
-
-            if(obj.positionCluster.length !== obj.canContainNumbers.size){ continue; }
-            
-            console.log(obj);
-
-            if( obj.positionCluster.length === 1) {
-                singles.push({
-                  square: groupIndeces[obj.type](obj.index)[
-                    obj.positionCluster[0]
-                  ],
-                  type: obj.type,
-                  number: [...obj.canContainNumbers][0],
-                  because: `only place this number can go on this ${obj.type}`,
-                });
-            } else { 
-                let type = obj.type.split('.')[0];
-                let related = groupIndeces[type](obj.index).map((idx: number)=> ({ idx, vals: possibles.get(idx) })); 
-                let canRemoveInnerFind = related.filter((i: { idx:number, vals: Set<number> | undefined }, idx: number) =>{
-                    if(i.vals && obj.positionCluster.includes(idx)){
-                        let trim = [...i.vals].filter(i=>[...obj.canContainNumbers].includes(i));
-                        if(i.vals && trim.length){
-                            return true;
-                        }
-                    }
-                }) as Array<{ idx:number, vals: Set<number>}>;
-                let canRemoveInner = canRemoveInnerFind.map((i: { idx:number, vals: Set<number> })=>({
-                    idx: i.idx,
-                    vals: [...i.vals].filter(i=>![...obj.canContainNumbers].includes(i))
-                }));
-                let canRemoveOuter = related.filter((i: { idx:number, vals: Set<number> | undefined }, idx: number)=>{
-                    if(i.vals && !obj.positionCluster.includes(idx)){
-                        let trim = [...i.vals].filter(i=>[...obj.canContainNumbers].includes(i));
-                        if(trim.length){
-                            return true;
-                        }
-                    }
-                });
-                groups.push({ ...obj, related, canRemoveInner, canRemoveOuter });
+      if (obj.positionCluster.length > 1 && obj.type.indexOf("num") === -1) {
+        let type = obj.type.split(".")[0];
+        let related = groupIndeces[type](obj.index).map((idx: number) => ({
+          idx,
+          vals: possibles.get(idx),
+        }));
+        let canRemoveInnerFind = related.filter(
+          (i: { idx: number; vals: Set<number> | undefined }, idx: number) => {
+            if (i.vals && obj.positionCluster.includes(idx)) {
+              let trim = [...i.vals].filter((i) =>
+                [...obj.canContainNumbers].includes(i)
+              );
+              if (i.vals && trim.length) {
+                return true;
+              }
             }
-        }
+          }
+        ) as Array<{ idx: number; vals: Set<number> }>;
+        let canRemoveInner = canRemoveInnerFind.map(
+          (i: { idx: number; vals: Set<number> }) => ({
+            idx: i.idx,
+            vals: [...i.vals].filter(
+              (i) => ![...obj.canContainNumbers].includes(i)
+            ),
+          })
+        );
+        let canRemoveOuter = related.filter(
+          (i: { idx: number; vals: Set<number> | undefined }, idx: number) => {
+            if (i.vals && !obj.positionCluster.includes(idx)) {
+              let trim = [...i.vals].filter((i) =>
+                [...obj.canContainNumbers].includes(i)
+              );
+              if (trim.length) {
+                return true;
+              }
+            }
+          }
+        );
+        groups.push({ ...obj, related, canRemoveInner, canRemoveOuter });
+      }
     }
+  }
 
-    //func canReject/omit = getBlocked(groups);
-    let deletors:StringMap = {};
-    let deletorsAll: Array<{ idx: number, nums: Array<number>, because: string}> = [];
-    // groups
-    /*clusters.values.forEach(i=>{
+  //func canReject/omit = getBlocked(groups);
+  let deletors: StringMap = {};
+  let deletorsAll: Array<{
+    idx: number;
+    nums: Array<number>;
+    because: string;
+  }> = [];
+  // groups
+  /*clusters.values.forEach(i=>{
         if(i.numbers.size >= 1) {
             console.log(i, i.canRemoveInner);
             i.canRemoveInner && i.canRemoveInner.forEach((ii:StringMap)=>{
@@ -102,9 +138,9 @@ export const getGroupClusters = (possibles:  ReadonlyMap<number, Set<number>>, g
             });
         }
     });*/
-    //console.log(clusters.values);
-    return {
-        groups,
-        singles
-    }
- }
+  //console.log(clusters.values);
+  return {
+    groups,
+    singles,
+  };
+};
