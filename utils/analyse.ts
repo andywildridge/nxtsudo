6a08dfcd366fb8a2d3beb5f567cb8de18e7480ad
andrew wildridge <andywildridge@andrews-imac.home>
2023-01-25 22:38:14 +0000

    added Single type

--- utils/analyse.ts
+++ utils/analyse.ts
@@ -1,65 +1,44 @@
-// import { indexToRow, indexToCol, indexToBox, indexToSegH, indexToSegV, groupIndeces } from './indexTransforms'
-// import { Collections, CollectionsGroup } from './collections';
 import { sortPossibles } from './sortPossibles';
 import { getSegementDeletors } from './segmentSkewers';
 import { getGroupClusters } from './getGroupClusters';
 import { findSquareSolvable } from './findSolvable';
 
 export const analyse = (possibles: ReadonlyMap<number, Set<number>>) => {
-    //pss.set(1, new Set([1]))
-    /////////////////////////////////////////////////////////////////////////////////////////////
 
-    //loop squares
-    //function find group possibles
-    // add to row, col, box, seg groups
-    //group.add('row.1.num.2',3);
-    //group.add('seg.1.num.2',3);
-    // can filter singles/only number possible in col, square, box groups (but no need as grouping? - no only check doubles? tidier for highlighting removables)
-    //function outs = getRCBs(pss) / segs = getSegs(pss)
-
-    //console.log(outs.values);
-    //console.log(segs);
-    //function find seg skewers
     const { groups, segments } = sortPossibles(possibles);
-
     const solvable = findSquareSolvable(possibles);
-
     const segmentRemovers = getSegementDeletors(possibles, segments);
-
     const clusterRemovers = getGroupClusters(possibles, groups);
 
-    /*clusterRemovers.singles.forEach(i=>{
-        solvable.push({
-            key: i.index,
-            item: new Set(i.canContainNumbers)
-        });
-    });*/
-
-    interface StringMap {
-        [key: string]: any;
+    interface Single {
+        square: number;
+        number: number;
+        because: string;        
     }
 
-    const singles: StringMap = {};
+    const singles:Record<number, Single[]> = {};
 
-    solvable.forEach((i: any)=>{
+    //square singles
+    solvable.forEach((i: Single)=>{
         if(!singles[i.square]){
         singles[i.square] = [];
         }
         singles[i.square].push(i);
     });
 
-    clusterRemovers.singles.forEach((i: any) => {
+    //group singles
+    clusterRemovers.singles.forEach((i: Single) => {
         if (!singles[i.square]) {
         singles[i.square] = [];
         }
         singles[i.square].push(i);
     });
 
-    console.log(singles);
+    console.log(clusterRemovers);
 
     return {
         solvable: singles,
-        removable: undefined
+        removable: clusterRemovers
     }
 
 }
